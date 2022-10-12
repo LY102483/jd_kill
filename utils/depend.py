343f02ab8d706d871810efff7c42ec08766f54b9
LiuYang <1024839103@qq.com>
2022-10-12 19:57:09 +0800

    完结撒花

--- utils/depend.py
+++ utils/depend.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 
 from utils.cookieUtil import checkCookie, mainPath, jobName
+from selenium.webdriver.chrome.service import Service as ChromeService # Similar thing for firefox also!
+from subprocess import CREATE_NO_WINDOW # This flag will only be available in windows
+
 
 
 class ChromeBrowser:
     def __init__(self):
         chrome_options=self.prepareChrome()
+        chrome_service = ChromeService(mainPath + "/driver/chromedriver.exe")
+        chrome_service.creationflags=CREATE_NO_WINDOW
         # 告诉编译器chromedriver在哪个位置并注册(如更换驱动版本则需要进行修改)
-        self.chrome = webdriver.Chrome(mainPath + "/utils/chromedriver",
-                                       chrome_options=chrome_options)
+        self.chrome = webdriver.Chrome(mainPath + "/driver/chromedriver.exe",
+                                       chrome_options=chrome_options,service=chrome_service)
 
         # 设置窗口大小和位置
         self.chrome.set_window_size(390, 884)
@@ -34,11 +39,13 @@ class ChromeBrowser:
             # 不加载图片, 提升速度
             chrome_options.add_argument('--blink-settings=imagesEnabled=false')
             # 不打开浏览器窗口
-            # chrome_options.add_argument("headless")
+            chrome_options.add_argument("headless")
             # 修改加载策略为eager：等待初始HTML文档完全加载和解析，并放弃css、图像和子框架的加载。
             # chrome_options.page_load_strategy='eager'
 
         # chrome_options.add_argument("headless")
         # 隐藏正受到自动测试软件的控制。
         chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+
         return chrome_options
